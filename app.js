0015364270dba77716f5b2d3338de7131e6eb7a5
cAtshaw <swiftj101@hotmail.com>
2023-06-01 15:55:02 +0100

    Result function added

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const resultDisplay = document.getElementById("result")
 const possibleChoices = document.querySelectorAll("button")
 let userChoice
 let computerChoice
+let result
 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener("click", (e) => {
     userChoice = e.target.id
     userChoiceDisplay.innerHTML = userChoice
     generateComputerChoice()
+    getResult()
 }))
 
 function generateComputerChoice() {
@@ -24,4 +26,30 @@ function generateComputerChoice() {
         computerChoice = "paper"
     }
     computerChoiceDisplay.innerHTML = computerChoice
+}
+
+function getResult() {
+    if (computerChoice === userChoice) {
+        return = "It's a Draw!"
+    }
+    if (computerChoice === "rock" && userChoice === "paper") {
+        return = "You win!"
+    }
+    if (computerChoice === "rock" && userChoice === "scissors") {
+        return = "You lost!"
+    }
+    if (computerChoice === "paper" && userChoice === "scissors") {
+        return = "You win!"
+    }
+    if (computerChoice === "paper" && userChoice === "rock") {
+        return = "You lost!"
+    }
+    if (computerChoice === "scissors" && userChoice === "rock") {
+        return = "You win!"
+    }
+    if (computerChoice === "scissors" && userChoice === "paper") {
+        return = "You lose!"
+    }
+
+    resultDisplay.innerHTML = result
 }
